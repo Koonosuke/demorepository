f9157df191c4a866db6bbcf200426e71e57b5ee1
81809 <81809@192.168.11.3>
2024-02-21 10:23:20 +0900

    初回コミット

---
+++ Githubkoukai/src/main/java/com/exampled/demo/GithubkoukaiApplication.java
@@ -0,0 +1,13 @@
+package com.exampled.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubkoukaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubkoukaiApplication.class, args);
+	}
+
+}
